61fbdefda54f2da3bf126cd3004c9ebe984e4ac3
Ankur Singh <as.ankursingh3.1@gmail.com>
2025-05-04 08:43:02 -0700

    continue chatting

--- src/web/chat.py
+++ src/web/chat.py
@@ -1,8 +1,11 @@
+import json
 from pathlib import Path
 
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+
+from src.core.deps import DBClientDep
 
 router = APIRouter()
 
@@ -10,13 +13,27 @@ router = APIRouter()
 templates = Jinja2Templates(directory=str(Path(__file__).parent.parent / "templates"))
 
 
+@router.get("/new", response_class=HTMLResponse)
+async def new_chat(request: Request, supabase_client: DBClientDep):
+    response = supabase_client.table("chat_history").insert({}).execute()
+    chat = {"id": response.data[0]["id"], "messages": []}
+    return templates.TemplateResponse("chat.html", {"request": request, "chat": chat})
+
+
 @router.get("/{chat_id}", response_class=HTMLResponse)
-async def chat(chat_id: str, request: Request):
+async def chat(chat_id: str, request: Request, supabase_client: DBClientDep):
+    response = (
+        supabase_client.table("chat_history").select("*").eq("id", chat_id).execute()
+    )
     messages = [
         {
-            "role": "assistant",
-            "content": "Hello! How can I assist you today?",
-        },
+            "role": m["role"],
+            "content": json.dumps(m["content"], ensure_ascii=False)[1:-1],
+        }
+        for m in response.data[0]["messages"]
     ]
-    chat = {"id": chat_id, "messages": messages}
+    chat = {
+        "id": chat_id,
+        "messages": messages[::-1],
+    }
     return templates.TemplateResponse("chat.html", {"request": request, "chat": chat})
